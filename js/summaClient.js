5190cdc30c7dadad10e3ce62a9eb581723175d54
jtren <jeana.ren@arcor.de>
2016-05-17 22:31:36 +0200

    duplicate predicates no longer show more than once

--- js/summaClient.js
+++ js/summaClient.js
@@ -81,7 +81,7 @@ function summa(uri, topK, language, fixedProperty, id, service) {
 									"subject" : "",
 									"predicate" : "",
 									"object" : "",
-									"rankValue" : 0.0,
+									"rankValue" : 0.0
 								};
 								statement["subject"] = data[keys[k]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#subject"][0]["@id"];
 								statement["predicate"] = data[keys[k]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate"][0]["@id"];
@@ -100,23 +100,102 @@ function summa(uri, topK, language, fixedProperty, id, service) {
 
 			//sort statements by rankValue
 			function compare(a, b) {
-				if (a["rankValue"] < b["rankValue"])
+				if (parseFloat(a["rankValue"]) < parseFloat(b["rankValue"])) {
 					return 1;
-				else if (a["rankValue"] > b["rankValue"])
+				} else if (parseFloat(a["rankValue"]) > parseFloat(b["rankValue"])) {
 					return -1;
-				else
+				} else {
 					return 0;
-			}
-			print["statements"].sort(compare);
-
-			$("#" + id).append("<div style='float:right' id='" + id + "_close'>x</div><h2>" + label(print.entity) + "</h2><table></table>");
-			for ( i = 0; i < print.statements.length; i++) {
-				if (print.statements[i].subject == print.entity) {
-					$("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + "&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\" id=\"" + print.statements[i].object + "\" href=\"#" + print.statements[i].object + "\">" + label(print.statements[i].object) + "</a></td></tr>");
-				} else if (print.statements[i].object == print.entity) {
-					$("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + " of&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\"id=\"" + print.statements[i].subject + "\" href=\"#" + print.statements[i].subject + "\">" + label(print.statements[i].subject) + "</a></td></tr>");
 				}
 			}
+
+
+			print["statements"].sort(compare);
+
+			var all_predicates = [];
+
+			var pred_list = {
+				"predicates" : []
+			};
+
+			//get every unique predicate
+			for (var i = 0; i < print["statements"].length; i++) {
+				var found = jQuery.inArray(print["statements"][i]["predicate"], all_predicates);
+				if (found >= 0) {
+					// Element was found
+				} else {
+					// Element was not found, add it.
+					all_predicates.push(print["statements"][i]["predicate"]);
+				}
+			}
+
+			//push objects to respective predicates
+			for (var k = 0; k < all_predicates.length; k++) {
+				var total_rank = 0.0;
+				var counter = 0.0;
+				var predicate = {
+					"pred" : "",
+					"obj" : [],
+					"subj" : [],
+					"rnk" : 0.0
+				};
+				predicate["pred"] = all_predicates[k];
+				for (var i = 0; i < print["statements"].length; i++) {
+					if (all_predicates[k] == print["statements"][i]["predicate"]) {
+						predicate.obj.push(print["statements"][i]["object"]);
+						predicate.subj.push(print["statements"][i]["subject"]);
+						total_rank += parseFloat(print["statements"][i]["rankValue"]);
+						counter++;
+					}
+				}
+				predicate["rnk"] = total_rank / counter;
+				pred_list.predicates.push(predicate);
+			}
+
+			function compare2(a, b) {
+				if (parseFloat(a["rnk"]) < parseFloat(b["rnk"])) {
+					return 1;
+				} else if (parseFloat(a["rnk"]) > parseFloat(b["rnk"])) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+			pred_list["predicates"].sort(compare2);
+
+			/* for (var i = 0; i < print["statements"].length; i++) {
+			console.log(print["statements"][i]["predicate"] + " " + print["statements"][i]["subject"] + " " + print["statements"][i]["rankValue"]);
+			}
+
+			for (var i = 0; i < pred_list["predicates"].length; i++) {
+			console.log(pred_list["predicates"][i]["pred"] + " " + pred_list["predicates"][i]["subj"] + " " + pred_list["predicates"][i]["rnk"]);
+			} */
+
+			//add to html
+			$("#" + id).append("<div style='float:right' id='" + id + "_close'>x</div><h2>" + label(print.entity) + "</h2><table></table>");
+			for (i = 0; i < all_predicates.length; i++) {
+				if (pred_list["predicates"][i]["subj"][0] == print.entity) {
+					$("#" + id).children("table").append("<tr><td>" + label(pred_list["predicates"][i]["pred"]) + "&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\" id=\"" + pred_list["predicates"][i]["obj"][0] + "\" href=\"#" + pred_list["predicates"][i]["obj"][0] + "\">" + label(pred_list["predicates"][i]["obj"][0]) + "</a></td></tr>");
+					//for each row after the first: without first column;
+					for (m = 1; m < pred_list["predicates"][i]["obj"].length; m++) {
+						$("#" + id).children("table").append("<tr><td>" + "<td><a class=\"" + id + " " + "click\" id=\"" + pred_list["predicates"][i]["obj"][m] + "\" href=\"#" + pred_list["predicates"][i]["obj"][m] + "\">" + label(pred_list["predicates"][i]["obj"][m]) + "</a></td></tr>");
+					}
+				} else if (pred_list["predicates"][i]["obj"][0] == print.entity) {
+					$("#" + id).children("table").append("<tr><td>" + label(pred_list["predicates"][i]["pred"]) + "&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\" id=\"" + pred_list["predicates"][i]["subj"][0] + "\" href=\"#" + pred_list["predicates"][i]["subj"][0] + "\">" + label(pred_list["predicates"][i]["subj"][0]) + "</a></td></tr>");
+					//for each row after the first: without first column;
+					for (m = 1; m < pred_list["predicates"][i]["subj"].length; m++) {
+						$("#" + id).children("table").append("<tr><td>" + "<td><a class=\"" + id + " " + "click\" id=\"" + pred_list["predicates"][i]["subj"][m] + "\" href=\"#" + pred_list["predicates"][i]["subj"][m] + "\">" + label(pred_list["predicates"][i]["subj"][m]) + "</a></td></tr>");
+					}
+				}
+			}
+
+			/* for ( i = 0; i < print.statements.length; i++) {
+			 if (print.statements[i].subject == print.entity) {
+			 $("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + "&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\" id=\"" + print.statements[i].object + "\" href=\"#" + print.statements[i].object + "\">" + label(print.statements[i].object) + "</a></td></tr>");
+			 } else if (print.statements[i].object == print.entity) {
+			 $("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + " of&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\"id=\"" + print.statements[i].subject + "\" href=\"#" + print.statements[i].subject + "\">" + label(print.statements[i].subject) + "</a></td></tr>");
+			 }
+			 } */
 			$("#" + id).append("<i style='font-size:10px'>_______<br>Summary by <a href='" + service.substring(0, service.lastIndexOf("/")) + "'>" + service.substring(0, service.lastIndexOf("/")) + "</a></i>");
 			$("#" + id).show();
 			$("#" + id + "_close").click(function() {
